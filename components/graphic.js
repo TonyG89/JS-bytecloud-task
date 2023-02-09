1da8d66d4fdcac3b7a92bfebf2ac4398c5d47185
Tony-tuf15 <a.gryhoryev@gmail.com>
2023-02-09 15:59:40 +0200

    done

--- components/graphic.js
+++ components/graphic.js
@@ -5,15 +5,15 @@ import mobile from "../helper/mobile.js";
 const graphic = (company) => {
     let price = calc(company)
 
-    const widthDiv = mobile(130,100) / 74 * price
+    const widthDiv = mobile(130,390) / 74 * price
 
     return (`
     <div class="oneline flex items-center 
-    ${mobile('flex-col-reverse w-1/5')}">
+    ${mobile('flex-col-reverse w-1/4')}">
 
         <div class="title w-1/5 
         ${mobile('h-[50px] px-1 w-full text-center')}">
-            <h1 class="text-2xl pb-1 ${mobile('text-[1.2rem]')}">${company.name.toUpperCase()}</h1>
+            <h1 class="text-2xl pb-1 ${mobile('text-[1rem]')}">${company.name.toUpperCase()}</h1>
             <div class="options flex text-xs justify-start ${mobile('text-[0.8rem] justify-center items-center')}">${radioDiv(company)}</div>
         </div>
 
